b2bf03aa46cd8fd9b69a741fdfeb84742100f670
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-12-20 21:57:08 +0530

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  transpilePackages: ['@clerk/nextjs'],
+  experimental: {
+    serverComponentsExternalPackages: ['@clerk/nextjs'],
+  },
 };
 
 export default nextConfig;
